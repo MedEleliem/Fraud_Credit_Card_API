34e95cbfff83488e698d8e55832b3f81e714109d
MedEleliem <64113527+MedEleliem@users.noreply.github.com>
2021-05-24 19:30:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def predict_api():
     output = prediction[0]
     return jsonify(output)
 
-if __name__ == "__main__" :
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
